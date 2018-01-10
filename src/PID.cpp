ca93e845f4345391068d59a37840f81fc1afea8d
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-09 21:39:48 -0600

    initial PID controller implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this::Kp = Kp;
+	this::Ki = Ki;
+	this::Kd = Kd;
+	this::p_error = 0.0;
+	this::i_error = 0.0;
+	this::d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = Kd*(cte - (p_error/Kp));
+	p_error = -Kp*cte;
+	i_error += Ki*cte;
 }
 
 double PID::TotalError() {
+	return p_error + i_error + d_error;
 }
 
