fa798bb1491cac3d615254a13a194075d248bd2a
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-10 21:21:08 -0600

    tweak PID controller gains

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double Kp = 1.0;
-  double Ki = 1.0;
-  double Kd = 1.0;
+  double Kp = 0.1;
+  double Ki = 0.0;
+  double Kd = 0.1;
   pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
@@ -74,7 +74,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.1;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
