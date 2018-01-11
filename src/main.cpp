c620e34bd3554fa5e4a988994a7760c4e762be7e
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-10 21:35:40 -0600

    tweak PID controller gains

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double Kp = 0.01;
+  double Kp = 1.0;
   double Ki = 0.001;
-  double Kd = 0.1;
+  double Kd = 5.0;
   pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
