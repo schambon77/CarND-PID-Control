6dfcbef22f50841502384da467e600857f779791
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-01-10 22:01:07 -0600

    tweak PID controller gains

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ int main()
   double Kd_t = 5.0;
   pid_t.Init(Kp_t, Ki_t, Kd_t);
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+  h.onMessage([&pid, &pid_t](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
